d40af34c66f9d15f839a584557a7cef8f6af9374
SENARATHNE-HDM <dewsjgh@gmail.com>
2026-06-11 16:47:45 +0530

    destroy: remove ec2 instance via pipeline

--- main.tf
+++ main.tf
@@ -11,12 +11,3 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
-
-resource "aws_instance" "cicd_server" {
-  ami           = "ami-0c7217cdde317cfec" # Ubuntu 22.04 LTS
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "DevOps-Project5-CICD-Server"
-  }
-}
